5c6b8e68e2da2d49f40cb99a4cc4d11fd25a30ea
Nelson Osacky <nelson@osacky.com>
2019-12-19 15:59:29 +0100

    Bump Dependencies. Improve error message for JAVA_HOME check. (#22)
    Gradle 6.0, Dagger, Kotlin, etc

--- doctor-plugin/build.gradle.kts
+++ doctor-plugin/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `kotlin-dsl`
-    kotlin("jvm") version "1.3.50"
+    kotlin("jvm") version "1.3.61"
     id("com.gradle.plugin-publish") version "0.10.1"
     id("org.jmailen.kotlinter") version "2.1.1"
     `maven-publish`
@@ -15,7 +15,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
+    compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
     implementation("io.reactivex.rxjava3:rxjava:3.0.0-RC1")
     testImplementation(gradleTestKit())
     testImplementation("junit:junit:4.12")
--- doctor-plugin/src/test/java/com/osacky/doctor/PluginIntegrationTest.kt
+++ doctor-plugin/src/test/java/com/osacky/doctor/PluginIntegrationTest.kt
@@ -19,7 +19,7 @@ class PluginIntegrationTest constructor(private val version: String) {
         @JvmStatic
         @Parameterized.Parameters(name = "{0}")
         fun getParams(): List<String> {
-            return listOf("5.0", "5.1", "5.2", "5.3", "5.4", "5.5.1", "5.6")
+            return listOf("5.0", "5.1", "5.3", "5.6", "6.0.1")
         }
     }
 
