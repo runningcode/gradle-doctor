89808f6b9bf73efdaaf84ff5e5f44d5bc879fffc
Nelson Osacky <nelson@osacky.com>
2020-01-21 09:29:12 +0100

    Bump versions

--- dagger-kapt/build.gradle.kts
+++ dagger-kapt/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 }
 
 dependencies {
-  implementation("com.google.dagger:dagger:2.25.3")
-  kapt("com.google.dagger:dagger-compiler:2.25.3")
+  implementation("com.google.dagger:dagger:2.25.4")
+  kapt("com.google.dagger:dagger-compiler:2.25.4")
 
   implementation("com.google.auto.value:auto-value-annotations:1.7")
   kapt("com.google.auto.value:auto-value:1.7")
