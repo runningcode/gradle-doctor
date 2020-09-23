ff5eeae0cb9489fc471dad2bba4b3aa65c85c907
Nelson Osacky <nelson@osacky.com>
2020-09-23 22:41:38 +0200

    Add warning when Jetifier is enabled. (#118)
    * Add warning when Jetifier is enabled.
    
    Fixes #82

---
+++ doctor-plugin/src/main/java/com/osacky/doctor/JetifierWarning.kt
@@ -0,0 +1,33 @@
+package com.osacky.doctor
+
+import org.gradle.api.Project
+
+class JetifierWarning(private val doctorExtension: DoctorExtension, private val project: Project) : BuildStartFinishListener {
+
+    override fun onStart() {
+    }
+
+    override fun onFinish(): List<String> {
+        val isJetifierEnabled = project.findProperty(jetifierGradleProperty) as String?
+        if (doctorExtension.warnWhenJetifierEnabled.get() && isJetifierEnabled != null && isJetifierEnabled == "true") {
+            return listOf(
+                """
+                Jetifier was enabled which means your builds are slower by 4-20%.
+                Here's an article to help you disable it:
+                https://adambennett.dev/2020/08/disabling-jetifier/
+
+                To disable this warning, configure the Gradle Doctor extension:
+                doctor {
+                  warnWhenJetifierEnable.set(false)
+                }
+                """.trimIndent()
+            )
+        } else {
+            return emptyList()
+        }
+    }
+
+    companion object {
+        const val jetifierGradleProperty = "android.enableJetifier"
+    }
+}
---
+++ doctor-plugin/src/test/java/com/osacky/doctor/JetifierWarningTest.kt
@@ -0,0 +1,71 @@
+package com.osacky.doctor
+
+import com.google.common.truth.Truth.assertThat
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+
+class JetifierWarningTest {
+
+    @get:Rule
+    val testProjectRoot = TemporaryFolder()
+
+    val jetifierWarning = "Jetifier was enabled which means your builds are slower by 4-20%."
+
+    @Test
+    fun testJetifierEnabledShowsWarning() {
+        testProjectRoot.writeBuildGradle(
+            """
+                    |plugins {
+                    |  id "com.osacky.doctor"
+                    |}
+                    |doctor {
+                    |  disallowMultipleDaemons = false
+                    |  javaHome {
+                    |    ensureJavaHomeMatches = false
+                    |  }
+                    |}
+                """.trimMargin("|")
+        )
+
+        val result = GradleRunner.create()
+            .forwardOutput()
+            .withArguments("help", "-Pandroid.enableJetifier=true")
+            .withPluginClasspath()
+            .withProjectDir(testProjectRoot.root)
+            .build()
+
+        assertThat(result.output).contains("BUILD SUCCESSFUL")
+        assertThat(result.output).contains("Gradle Doctor Prescriptions")
+        assertThat(result.output).contains(jetifierWarning)
+    }
+
+    @Test
+    fun testJetifierDisabledShowNoWarning() {
+        testProjectRoot.writeBuildGradle(
+            """
+                    |plugins {
+                    |  id "com.osacky.doctor"
+                    |}
+                    |doctor {
+                    |  disallowMultipleDaemons = false
+                    |  javaHome {
+                    |    ensureJavaHomeMatches = false
+                    |  }
+                    |}
+                """.trimMargin("|")
+        )
+
+        val result = GradleRunner.create()
+            .forwardOutput()
+            .withArguments("help", "-Pandroid.enableJetifier=false")
+            .withPluginClasspath()
+            .withProjectDir(testProjectRoot.root)
+            .build()
+
+        assertThat(result.output).contains("BUILD SUCCESSFUL")
+        assertThat(result.output).doesNotContain(jetifierWarning)
+        assertThat(result.output).doesNotContain("Gradle Doctor Prescriptions")
+    }
+}
