3fe49e02c532d42d6edaebae91645b576b4fd47f
Nick Firmani <NickFirmani@users.noreply.github.com>
2023-04-19 10:39:28 -0600

    Fix extra blank line

--- doctor-plugin/src/optimalGCTest/java/com/osacky/doctor/OptimalGCIntegrationTest.kt
+++ doctor-plugin/src/optimalGCTest/java/com/osacky/doctor/OptimalGCIntegrationTest.kt
@@ -23,7 +23,6 @@ class OptimalGCIntegrationTest : AbstractIntegrationTest() {
            """.trimMargin("|")
     }
 
-
     @Test
     fun testParallelGCWarningEnabled() {
         testProjectRoot.writeBuildGradle(
