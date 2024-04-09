98a236ff4ed3839fa46233ae6d487ee184e43027
Nelson Osacky <nelson@osacky.com>
2024-04-09 17:39:04 +0200

    Update to new DV plugin API

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,13 +6,13 @@ pluginManagement {
 }
 
 plugins {
-  id("com.gradle.enterprise") version "3.17.1"
+  id("com.gradle.develocity") version "3.17.1"
 }
 
-gradleEnterprise {
+develocity {
   buildScan {
-    termsOfServiceUrl = "https://gradle.com/terms-of-service"
-    termsOfServiceAgree = "yes"
+    termsOfUseUrl = "https://gradle.com/help/legal-terms-of-use"
+    termsOfUseAgree = "yes"
     publishAlways()
   }
 }
