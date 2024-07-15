02350c01e55a4735f04898e7d2ec1957e3390812
Nelson Osacky <nosacky@gradle.com>
2024-07-15 13:13:05 +0200

    fix other formatting issues

--- doctor-plugin/src/testFixtures/java/com/osacky/doctor/TestFixtures.kt
+++ doctor-plugin/src/testFixtures/java/com/osacky/doctor/TestFixtures.kt
@@ -4,5 +4,9 @@ import org.junit.rules.TemporaryFolder
 import java.io.File
 
 fun TemporaryFolder.setupFixture(fixtureName: String) {
-    File(this::class.java.classLoader.getResource(fixtureName)!!.file).copyRecursively(newFile(fixtureName), true)
+    File(
+        this::class.java.classLoader
+            .getResource(fixtureName)!!
+            .file,
+    ).copyRecursively(newFile(fixtureName), true)
 }
