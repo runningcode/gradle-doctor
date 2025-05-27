6d48882454a98da58d8171ef077316febd1d4409
Shivang Shah <sshah@block.xyz>
2025-05-27 09:47:47 -0400

    use isGradle65OrNewer() && !isGradle74OrNewer() check

--- doctor-plugin/src/main/java/com/osacky/doctor/DoctorPlugin.kt
+++ doctor-plugin/src/main/java/com/osacky/doctor/DoctorPlugin.kt
@@ -1,18 +1,8 @@
 package com.osacky.doctor
 
 import com.gradle.develocity.agent.gradle.adapters.BuildScanAdapter
-import com.osacky.doctor.internal.CliCommandExecutor
-import com.osacky.doctor.internal.Clock
-import com.osacky.doctor.internal.DaemonChecker
-import com.osacky.doctor.internal.DirtyBeanCollector
-import com.osacky.doctor.internal.FRESH_DAEMON
-import com.osacky.doctor.internal.IntervalMeasurer
-import com.osacky.doctor.internal.PillBoxPrinter
-import com.osacky.doctor.internal.SystemClock
-import com.osacky.doctor.internal.UnixDaemonChecker
-import com.osacky.doctor.internal.UnsupportedOsDaemonChecker
+import com.osacky.doctor.internal.*
 import com.osacky.doctor.internal.farthestEmptyParent
-import com.osacky.doctor.internal.shouldUseCoCaClasses
 import org.gradle.api.Action
 import org.gradle.api.GradleException
 import org.gradle.api.Plugin
@@ -165,10 +155,12 @@ class DoctorPlugin : Plugin<Project> {
     ): JavaHomeCheck {
         val jvmVariables =
             JvmVariables(
-                environmentJavaHomeProvider = project
-                    .providers
-                    .environmentVariable(JAVA_HOME)
-                    .forUseAtConfigurationTime(),
+                environmentJavaHomeProvider =
+                if (isGradle65OrNewer() && !isGradle74OrNewer()) {
+                    project.providers.environmentVariable(JAVA_HOME).forUseAtConfigurationTime()
+                } else {
+                    project.providers.environmentVariable(JAVA_HOME)
+                },
                 gradleJavaHome = Jvm.current().javaHome.path,
             )
         return JavaHomeCheck(jvmVariables, extension.javaHomeHandler, pillBoxPrinter)
