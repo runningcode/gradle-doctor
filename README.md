702544ab7263d621f603f838ea01840e023f3809
Clay Johnson <cjohnson@gradle.com>
2026-08-27 03:26:37 -0500

    Onboard to the OSS Community Develocity Instance (#495)

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Gradle Doctor
 
+[![Revved up by Develocity](https://img.shields.io/badge/Revved%20up%20by-Develocity-06A0CE?logo=Gradle&labelColor=02303A)](https://community.develocity.cloud/scans?search.rootProjectNames=gradle-doctor)
+
 The right prescription for your Gradle build.
 
 ### Documentation is at [runningcode.github.io/gradle-doctor](https://runningcode.github.io/gradle-doctor)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,13 +6,36 @@ pluginManagement {
 }
 
 plugins {
-  id("com.gradle.develocity") version "4.4.1"
+  id("com.gradle.develocity") version "4.5.0"
+  id("com.gradle.common-custom-user-data-gradle-plugin") version "2.8.0"
 }
 
+val isCI = System.getenv("CI") != null
+
+rootProject.name = "gradle-doctor"
+
 develocity {
+  server = "https://community.develocity.cloud"
+  projectId = "runningcode"
   buildScan {
-    termsOfUseUrl = "https://gradle.com/help/legal-terms-of-use"
-    termsOfUseAgree = "yes"
+    uploadInBackground = !isCI
+    publishing.onlyIf { it.isAuthenticated }
+    obfuscation {
+      ipAddresses { addresses -> addresses.map { _ -> "0.0.0.0" } }
+    }
+  }
+}
+
+buildCache {
+  local {
+    isEnabled = true
+  }
+
+  remote(develocity.buildCache) {
+    isEnabled = true
+    // Check access key presence to avoid build cache errors on PR builds when access key is not present
+    val accessKey = System.getenv("DEVELOCITY_ACCESS_KEY")
+    isPush = isCI && accessKey != null
   }
 }
 
